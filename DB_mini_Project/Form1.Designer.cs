ee8e21310bdd56f05747120135a1642437089093
will5810 <will5810@naver.com>
2021-04-01 11:38:42 +0900

    필터기능추가

--- DB_mini_Project/Form1.Designer.cs
+++ DB_mini_Project/Form1.Designer.cs
@@ -29,234 +29,144 @@ namespace DB_mini_Project
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button2 = new System.Windows.Forms.Button();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
-            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.flowLayoutPanel3 = new System.Windows.Forms.FlowLayoutPanel();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.flowLayoutPanel4 = new System.Windows.Forms.FlowLayoutPanel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.AccessibleDescription = "";
+            this.textBox1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.textBox1.Location = new System.Drawing.Point(124, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(319, 21);
+            this.textBox1.TabIndex = 10;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(531, 258);
+            this.button1.Location = new System.Drawing.Point(458, 24);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(122, 37);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "불러오기";
+            this.button1.Size = new System.Drawing.Size(98, 31);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "검색";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // openFileDialog1
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
-            // textBox1
+            // flowLayoutPanel3
             // 
-            this.textBox1.Location = new System.Drawing.Point(52, 127);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(213, 24);
-            this.textBox1.TabIndex = 3;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(502, 67);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(78, 31);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
-            "귀금속",
-            "독서실",
-            "동물병원",
-            "떡",
-            "문구",
-            "문화,스포츠",
-            "미용",
-            "반찬",
-            "병원",
-            "사진관",
-            "산후도우미",
-            "산후조리원",
-            "생협",
-            "서점",
-            "세탁소",
-            "슈퍼마켓",
-            "시산하기관(구내식당 등)",
-            "안경점",
-            "약국",
-            "육아용품",
-            "음식점",
-            "정육점",
-            "제과제빵",
-            "카센타",
-            "카페",
-            "키즈카페",
-            "택시",
-            "학원",
-            "한의원",
-            "헬스장"});
-            this.comboBox1.Location = new System.Drawing.Point(52, 78);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(92, 20);
-            this.comboBox1.TabIndex = 5;
-            // 
-            // comboBox2
-            // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Items.AddRange(new object[] {
-            "분당구",
-            "수정구",
-            "중원구"});
-            this.comboBox2.Location = new System.Drawing.Point(163, 78);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(92, 20);
-            this.comboBox2.TabIndex = 6;
-            // 
-            // comboBox3
-            // 
-            this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Items.AddRange(new object[] {
-            "분당구--",
-            "구미1동",
-            "구미동",
-            "금곡동",
-            "백현동",
-            "분당동",
-            "삼평동",
-            "서현1동",
-            "서현2동",
-            "수내1동",
-            "수내2동",
-            "수내3동",
-            "야탑1동",
-            "야탑2동",
-            "야탑3동",
-            "운중동",
-            "이매1동",
-            "이매2동",
-            "정자1동",
-            "정자2동",
-            "정자3동",
-            "판교동",
-            "",
-            "중원구--",
-            "금광1동",
-            "금광2동",
-            "도촌동",
-            "상대원1동",
-            "상대원2동",
-            "상대원3동",
-            "성남동",
-            "은행1동",
-            "은행2동",
-            "중앙동",
-            "하대원동",
-            "",
-            "수정구--",
-            "고등동",
-            "단대동",
-            "복정동",
-            "산성동",
-            "수진1동",
-            "수진2동",
-            "시흥동",
-            "신촌동",
-            "신흥1동",
-            "신흥2동",
-            "신흥3동",
-            "양지동",
-            "태평1동",
-            "태평2동",
-            "태평3동",
-            "태평4동"});
-            this.comboBox3.Location = new System.Drawing.Point(276, 78);
-            this.comboBox3.Name = "comboBox3";
-            this.comboBox3.Size = new System.Drawing.Size(92, 20);
-            this.comboBox3.TabIndex = 7;
+            this.flowLayoutPanel3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.flowLayoutPanel3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.flowLayoutPanel3.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.flowLayoutPanel3.Location = new System.Drawing.Point(49, 272);
+            this.flowLayoutPanel3.Name = "flowLayoutPanel3";
+            this.flowLayoutPanel3.Size = new System.Drawing.Size(804, 76);
+            this.flowLayoutPanel3.TabIndex = 12;
             // 
             // label1
             // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("굴림", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label1.Location = new System.Drawing.Point(50, 48);
+            this.label1.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(49, 35);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(37, 15);
-            this.label1.TabIndex = 8;
-            this.label1.Text = "품목";
+            this.label1.Size = new System.Drawing.Size(59, 16);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "상호명";
             // 
-            // label2
+            // flowLayoutPanel4
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("굴림", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label2.Location = new System.Drawing.Point(161, 48);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(22, 15);
-            this.label2.TabIndex = 9;
-            this.label2.Text = "구";
+            this.flowLayoutPanel4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.flowLayoutPanel4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.flowLayoutPanel4.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.flowLayoutPanel4.Location = new System.Drawing.Point(420, 84);
+            this.flowLayoutPanel4.Name = "flowLayoutPanel4";
+            this.flowLayoutPanel4.Size = new System.Drawing.Size(351, 76);
+            this.flowLayoutPanel4.TabIndex = 13;
             // 
-            // label3
+            // listBox1
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("굴림", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label3.Location = new System.Drawing.Point(274, 48);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(22, 15);
-            this.label3.TabIndex = 10;
-            this.label3.Text = "동";
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(500, 380);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(372, 352);
+            this.listBox1.TabIndex = 16;
             // 
-            // label4
+            // dataGridView1
             // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("굴림", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label4.Location = new System.Drawing.Point(382, 48);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(72, 15);
-            this.label4.TabIndex = 11;
-            this.label4.Text = "결제 방법";
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(49, 379);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(445, 361);
+            this.dataGridView1.TabIndex = 15;
             // 
-            // comboBox4
+            // flowLayoutPanel2
             // 
-            this.comboBox4.FormattingEnabled = true;
-            this.comboBox4.Items.AddRange(new object[] {
-            "모두",
-            "지류",
-            "모바일"});
-            this.comboBox4.Location = new System.Drawing.Point(384, 78);
-            this.comboBox4.Name = "comboBox4";
-            this.comboBox4.Size = new System.Drawing.Size(92, 20);
-            this.comboBox4.TabIndex = 12;
+            this.flowLayoutPanel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.flowLayoutPanel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.flowLayoutPanel2.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(52, 175);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(801, 80);
+            this.flowLayoutPanel2.TabIndex = 14;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.flowLayoutPanel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.flowLayoutPanel1.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(52, 84);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(351, 76);
+            this.flowLayoutPanel1.TabIndex = 11;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.comboBox4);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.comboBox3);
-            this.Controls.Add(this.comboBox2);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(920, 765);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.flowLayoutPanel3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.flowLayoutPanel4);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.flowLayoutPanel2);
+            this.Controls.Add(this.flowLayoutPanel1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -264,18 +174,16 @@ namespace DB_mini_Project
 
         #endregion
 
+        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.ComboBox comboBox2;
-        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel3;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel4;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
     }
 }
 
--- DB_mini_Project/Form1.cs
+++ DB_mini_Project/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,36 +17,267 @@ namespace DB_mini_Project
 {
     public partial class Form1 : Form
     {
+        List<String> location_list = new List<string>(); // "구" 데이터가 저장되는 변수
+        Dictionary<String, List<String>> location2_list = new Dictionary<string, List<string>>(); // "동" 데이터가 저장되는 변수, 각 "구" 별로 "동" 이 저장된다.
+        List<String> item_list = new List<string>(); // "품목" 데이터가 저장되는 변수
+        List<String> pay_list = new List<string>(); // "결제" 데이터가 저장되는 변수
+
+        List<Tuple<string, string>> search = new List<Tuple<string, string>>();
+
+        String location = ""; // "구" 를 저장하는 변수
+        String location2 = ""; // "동" 을 저장하는 변수
+        String item = ""; // "품목" 를 저장하는 변수
+        String pay = ""; // "결제" 를 저장하는 변수
+
+        Control[] BTN_location; // "구" 버튼을 생성하는 변수
+        Dictionary<String, Control[]> BTN_location2 = new Dictionary<string, Control[]>(); // "동" 변수를 생성하는 변수
+        Control[] BTN_item; // "품목" 버튼을 생성하는 변수
+        Control[] BTN_pay; // "결제" 버튼을 생성하는 변수
+
+        List<String[]> data = new List<String[]>(); // 엑셀 파일에서 읽어온 데이터를 저장하는 변수
+
         public Form1()
         {
             InitializeComponent();
+            textBox1.KeyDown += Enter_KeyDown;
         }
 
+        // 검색 버튼입니다.
         private void button1_Click(object sender, EventArgs e)
         {
-            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            listBox1.Items.Clear();
+
+
+            data.ForEach(d =>
             {
-                string filename = openFileDialog1.FileName;
-                DataTable dt1 = getDataTableFromCsv(filename, false); ;
+                if ((d[3].Equals(location) || location.Equals(""))
+                && (d[4].Equals(location2) || location2.Equals(""))
+                && (d[2].Equals(item) || item.Equals(""))
+                && (d[5].Equals(pay) || pay.Equals(""))
+                    && ((d[1].Contains(textBox1.Text)) || (d[4].Contains(textBox1.Text)) || (d[5].Contains(textBox1.Text))))
+                {
+                    listBox1.Items.Add(d[1] + " " + d[7] + Environment.NewLine);
+
+                    string store_name = d[1];
+                    string store_address = d[7];
+                    search.Add(new Tuple<string, string>(store_name, store_address));
+
+                }
+            });
+
+
+
+        }
+
+
+        // 데이터를 로드하고
+        // 검색할 데이터를 설정하는 부분입니다 
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            StreamReader sr = new StreamReader("File.csv", System.Text.Encoding.GetEncoding(949));
+            string buf = sr.ReadLine();
+            string[] sArr = buf.Split(',');
+
+
+            for (int i = 0; i < sArr.Length; i++)
+            {
+                String a = sArr[i];
+            }
+            while (true)
+            {
+                buf = sr.ReadLine();
+                if (buf == null) break;
+                sArr = buf.Split(',');
+                location_list.Add(sArr[3]);
+                item_list.Add(sArr[2]);
+                pay_list.Add(sArr[5]);
+                if (!location2_list.ContainsKey(sArr[3]))
+                {
+                    location2_list[sArr[3]] = new List<string>();
+                }
+                location2_list[sArr[3]].Add(sArr[4]);
+                data.Add(sArr);
+            }
+
+            location_list = location_list.Distinct().ToList();
+            item_list = item_list.Distinct().ToList();
+            pay_list = pay_list.Distinct().ToList();
+
+            foreach (String key in location_list)
+            {
+                location2_list[key] = location2_list[key].Distinct().ToList();
+                BTN_location2[key] = new Control[location2_list[key].Count];
+            }
+
+            BTN_location = new Control[location_list.Count];
+            BTN_item = new Control[item_list.Count];
+            BTN_pay = new Control[pay_list.Count];
+
+            for (int i = 0; i < location_list.Count; i++)
+            {
+                BTN_location[i] = new Button();
+                BTN_location[i].Name = i.ToString();
+                BTN_location[i].Parent = this;
+                BTN_location[i].Size = new Size(90, 30);
+                BTN_location[i].Text = location_list[i];
+
+                BTN_location[i].Click += Btn_location_Click;
+
+                flowLayoutPanel1.Controls.Add(BTN_location[i]);
+
+            }
+
+            for (int i = 0; i < item_list.Count; i++)
+            {
+                BTN_item[i] = new Button();
+                BTN_item[i].Name = i.ToString();
+                BTN_item[i].Parent = this;
+                BTN_item[i].Size = new Size(90, 30);
+                BTN_item[i].Text = item_list[i];
+
+                BTN_item[i].Click += Btn_item_Click;
+
+                flowLayoutPanel3.Controls.Add(BTN_item[i]);
+
+            }
+
+            for (int i = 0; i < pay_list.Count; i++)
+            {
+                BTN_pay[i] = new Button();
+                BTN_pay[i].Name = i.ToString();
+                BTN_pay[i].Parent = this;
+                BTN_pay[i].Size = new Size(90, 30);
+                BTN_pay[i].Text = pay_list[i];
+
+                BTN_pay[i].Click += Btn_pay_Click;
+
+                flowLayoutPanel4.Controls.Add(BTN_pay[i]);
+
+            }
+
+
+            clear_location_btn();
+            clear_item_btn();
+            clear_pay_btn();
+            //clear_location2_btn();
+
+            flowLayoutPanel1.AutoScroll = true;
+            flowLayoutPanel2.AutoScroll = true;
+            flowLayoutPanel3.AutoScroll = true;
+            flowLayoutPanel4.AutoScroll = true;
+
+
+            sr.Close();
+
+        }
+
+
+
+        // enter key 처리 부분입니다.
+        private void Enter_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                button1_Click(sender, e);
             }
         }
 
-        private DataTable getDataTableFromCsv(string path, bool isFirstRowHeader)
+
+        // "구" 버튼이 나오게 하는 작업입니다.
+        private void clear_location_btn()
         {
-            string header = isFirstRowHeader ? "Yes" : "No";
-            string pathOnly = Path.GetDirectoryName(path);
-            string fileName = Path.GetFileName(path);
-            string sql = @"SELECT * FROM [" + fileName + "]";
-            using (OleDbConnection connection = new OleDbConnection(
-                @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + pathOnly + ";Extended Properties=\"Text;HDR=" + header + "\""))
-            using (OleDbCommand command = new OleDbCommand(sql, connection))
-            using (OleDbDataAdapter adapter = new OleDbDataAdapter(command))
+            for (int i = 0; i < location_list.Count; i++)
             {
-                DataTable dataTable = new DataTable();
-                dataTable.Locale = CultureInfo.CurrentCulture;
-                adapter.Fill(dataTable);
-                return dataTable;
+                BTN_location[i].BackColor = Button.DefaultBackColor;
             }
+        }
+
+        // "동" 버튼이 나오게 하는 작업입니다.
+        private void clear_location2_btn()
+        {
+            for (int i = 0; i < location2_list[location].Count; i++)
+            {
+                BTN_location2[location][i].BackColor = Button.DefaultBackColor;
+            }
+        }
+
+        // "품목" 버튼이 나오게 하는 작업입니다.
+        private void clear_item_btn()
+        {
+            for (int i = 0; i < item_list.Count; i++)
+            {
+                BTN_item[i].BackColor = Button.DefaultBackColor;
+            }
+        }
+
+        // "결제 방법" 버튼이 나오게 하는 작업입니다.
+        private void clear_pay_btn()
+        {
+            for (int i = 0; i < pay_list.Count; i++)
+            {
+                BTN_pay[i].BackColor = Button.DefaultBackColor;
+            }
+        }
+
+
+        // "구"를 눌렀을때 그 안의 "동"이 나오도록 하는 작업 입니다.
+        private void add_location2_btn(String key)
+        {
+            for (int i = 0; i < location2_list[key].Count; i++)
+            {
+                BTN_location2[key][i] = new Button();
+                BTN_location2[key][i].Name = i.ToString();
+                BTN_location2[key][i].Parent = this;
+                BTN_location2[key][i].Size = new Size(90, 30);
+                BTN_location2[key][i].Text = location2_list[key][i];
+
+                BTN_location2[key][i].Click += Btn_location2_Click;
+
+                BTN_location2[key][i].BackColor = Button.DefaultBackColor;
+
+                flowLayoutPanel2.Controls.Add(BTN_location2[key][i]);
+            }
+        }
+
+        // "구" 단위 버튼을 눌렀을때 호출되는 함수입니다.
+        // "동" 을 초기화하고 해당 "구" 에 해당하는 "동" 을 새로 만듬
+        public void Btn_location_Click(object sender, EventArgs e)
+        {
+            location = ((Button)sender).Text;
+            location2 = "";
+            flowLayoutPanel2.Controls.Clear();
+            clear_location_btn();
+            add_location2_btn(location);
+            ((Button)sender).BackColor = Color.Gray;
+        }
+
+        // "동" 단위 버튼을 눌렀을때 호출되는 함수입니다.
+        public void Btn_location2_Click(object sender, EventArgs e)
+        {
+            location2 = ((Button)sender).Text;
+            clear_location2_btn();
+            ((Button)sender).BackColor = Color.Gray;
+        }
+
+        // "동" 단위 버튼을 눌렀을때 호출되는 함수입니다.
+        public void Btn_item_Click(object sender, EventArgs e)
+        {
+            item = ((Button)sender).Text;
+
+
+            clear_item_btn();
+            ((Button)sender).BackColor = Color.Gray;
+        }
+
+        // "동" 단위 버튼을 눌렀을때 호출되는 함수입니다.
+        public void Btn_pay_Click(object sender, EventArgs e)
+        {
+            pay = ((Button)sender).Text;
+
+
+            clear_pay_btn();
+            ((Button)sender).BackColor = Color.Gray;
         }
     }
 }
