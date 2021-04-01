914a8bcda6b4e2daad177a2772f1cc327de6c6b8
will5810 <will5810@naver.com>
2021-04-01 15:35:43 +0900

    Result

--- DB_mini_Project/Form1.Designer.cs
+++ DB_mini_Project/Form1.Designer.cs
@@ -166,6 +166,7 @@ namespace DB_mini_Project
             this.Controls.Add(this.flowLayoutPanel1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
